fddc22aa85af64c7018647dfa6d1870868dbc36c
Andrew Circelli <andrewcircelli@gmail.com>
2022-05-19 23:29:42 -0400

    move axios call out of try catch within services.js

--- services.js
+++ services.js
@@ -2,12 +2,16 @@ const apiKey = process.env.SPORTSDATA_KEY;
 const axios = require("axios");
 const config = require("./config");
 
+const axiosGetPreGameOddsByDate = async (date, apiKey) => {
+  const sportsDataResponse = await axios.get(
+    config.generatePreGameOddsByDateURL(date, apiKey)
+  );
+  return sportsDataResponse.data;
+};
 const getPreGameOddsByDate = async (req, res) => {
   try {
-    const sportsDataResponse = await axios.get(
-      config.generatePreGameOddsByDateURL(req.params.date, apiKey)
-    );
-    res.status(200).send(sportsDataResponse.data);
+    const data = await axiosGetPreGameOddsByDate(req.params.date, apiKey);
+    res.status(200).send(data);
   } catch (err) {
     res.status(400).send({ error: err });
   }
